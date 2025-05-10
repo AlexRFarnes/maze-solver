0d11e605daac5cfed43006606c0144f52c133a20
AlexRFarnes <alexrfarnes@gmail.com>
2025-05-10 11:26:29 +0800

    create point and line class

--- main.py
+++ main.py
@@ -1,32 +1,23 @@
-from tkinter import Tk, BOTH, Canvas
-
-
-class Window:
-    def __init__(self, width, height):
-        self.__root = Tk()
-        self.__root.title("Maze Solver")
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
-        self.__canvas = Canvas(self.__root, bg="white",
-                               width=width, height=height)
-        self.__canvas.pack(expand=1, fill=BOTH)
-        self.__running = False
-
-    def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
-
-    def wait_for_close(self):
-        self.__running = True
-        while self.__running:
-            self.redraw()
-        print("Window closed...")
-
-    def close(self):
-        self.__running = False
+from graphics import Window, Line, Point
 
 
 def main():
     win = Window(800, 600)
+
+    p1 = Point(50, 50)
+    p2 = Point(150, 300)
+    p3 = Point(500, 500)
+    p4 = Point(700, 40)
+    l1 = Line(p1, p2)
+    l2 = Line(p1, p4)
+    l3 = Line(p3, p4)
+    l4 = Line(p2, p3)
+
+    win.draw_line(l1, "red")
+    win.draw_line(l2, "orange")
+    win.draw_line(l3, "green")
+    win.draw_line(l4, "blue")
+
     win.wait_for_close()
 
 
